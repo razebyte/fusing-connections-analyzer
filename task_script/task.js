4e09cb68b0a5573e42d5c3ca2ff1cefa5da0db15
gaunsd <gam0929@gmail.com>
2018-11-25 00:38:36 -0500

    Task Script
    * Added what will be the TASK for this project. More work needs to be
    done to have this project be compliant with npm (ie: add a
    package.json), and further work to set up our use of node-bandwidth
    test.

---
+++ task_script/task.js
@@ -0,0 +1,21 @@
+/** 
+ * Task.js
+ *
+ * This will be a file that will contain the code that will execute a QoS Test.
+ * In this case this will be an HTTP GET request on a test file publicly available.
+ *
+ * Various Metricss will be gathered.
+ *
+ * The aim of this task is to enable this to occur over a specific HTTP Proxy to compare different metrics.
+ */
+
+
+var http = require("http");
+var fs = require("fs");
+var fork = require("child_process").fork;
+
+
+//TODO: Using Node, run ./libraries/node-bandwidth-tester/client.js against a given proxy
+
+var child = fork('./node-bandwidth-tester/client');
+
