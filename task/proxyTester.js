c7da62fe6b666d08a66b73bd721277f5355f2f11
Siavash Khan <savish.khan@gmail.com>
2018-11-29 16:56:00 -0500

    Functioning proxyTester.js with TaskRunner

--- task/proxyTester.js
+++ task/proxyTester.js
@@ -1,22 +1,25 @@
 // Author: Siavash Khan || RazeByte
 "use strict";
 
-var taskEnvironment = require('./environment.json');
 var TaskRunner = require('./lib/TaskRunner');
 
 // Step 1 start n proxies for all available configurations
 let DispatchCaller = require('./dispatchCaller.js');
 let dispatch = new DispatchCaller();
 
-var x, address, addrCombos, port;
+const fs = require('fs'); // file-system
+
+var x, address, addrCombos, port, runner;
 addrCombos = dispatch.getNetworkAddressCombinations();
 for (x = 0; x < addrCombos.length; x++) {
 	port = dispatch.getFreePort();
 	dispatch.startHTTPProxy(addrCombos[x], port);
+
+	// Step 2 Call TaskRunner with the modified
+	let rawData = fs.readFileSync("environment.json");
+	let taskEnvironment = JSON.parse(rawData);
+	taskEnvironment["proxy"]["port"] = port;
+	runner = new TaskRunner(taskEnvironment);
+	runner.execute();
 	console.log("\n");
 }
-
-
-// Step 2 Call TaskRunner with the modified
-var runner = new TaskRunner( taskEnvironment );
-runner.execute();
