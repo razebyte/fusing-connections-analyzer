be6127022479b7b1d2de663ef4c7e3e08f498e26
gaunsd <gam0929@gmail.com>
2018-11-28 22:34:48 -0500

    Random Output Generator
    * Added a js file to output random results of the schema of the output

---
+++ task/schema/randomOutputGenerator.js
@@ -0,0 +1,64 @@
+var fs = require('fs');
+
+if(process.argv.length !== 4) {
+	console.error(
+		"ERROR: Input 2 additional arguments.\n" + 
+		"\t(1 | int) number of Networks being tested\n" +
+		"\t(2 | int) number of number of times the test has been"
+	);
+}
+
+var numberOfNetworks = process.argv[2];
+var numberOfTimes = process.argv[3];
+
+
+
+
+
+var result = {};
+
+for(var i=0; i < numberOfNetworks; i++) {
+	var dateAndTests = {};
+
+	for(var j=0; j < numberOfTimes; j++) {
+		var modifiedDate = new Date();
+		modifiedDate.setHours(modifiedDate.getHours()+1);
+		dateAndTests[modifiedDate.toISOString()] = [
+			{
+				"test": "BandwidthTask",
+				"result": {
+					latency: (Math.random() * 1000),
+					dataSizeKB: (Math.random() * 1000),
+					avgThroughput: (Math.random() * 1000),
+					maxThroughput: (Math.random() * 1000),
+					minThroughput: (Math.random() * 1000)
+				}
+			}
+		]
+	}
+
+	result["Wifi" + i] = dateAndTests;
+}
+
+
+var schema = {
+	"NetworkIdentifier": {
+		"someDateInYYYY-MM-DD-HH-MMFormat": [
+			{
+				"test": "TheNameOfATest",
+				"result": {}
+			}
+		]
+	}
+};
+
+var stringResult = JSON.stringify(result, null, "\t");
+
+fs.writeFile(
+	"output.generator.json.tmp", 
+	stringResult, 
+	function(err) {
+		if(err) return console.log(err);
+		console.log("The generated file has been saved as: output.generator.json.tmp");
+	}
+); 
