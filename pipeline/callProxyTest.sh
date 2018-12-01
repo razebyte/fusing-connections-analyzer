228fae6624e5a04ad649c95b30704b7400ce5aed
Hussein Fahmy <fahmy.hussein@hotmail.com>
2018-12-01 02:39:05 -0500

    slight modifications to caller script

--- pipeline/callProxyTest.sh
+++ pipeline/callProxyTest.sh
@@ -4,4 +4,8 @@ cd ./git_repo/task/;
 npm install .
 /usr/local/bin/nodejs proxyTester.js;
 
-cat ./results.database.json
+cat ./results.database.json > results.log
+
+git add  ./results.database.json
+git commit -m "Data Update from Pipeline"
+git push
